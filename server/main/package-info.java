3f06e51516bbd01609ba3ce712b7ca64bbca83a3
João Fonseca <joao.fonseca@ua.pt>
2024-05-16 17:35:58 +0100

    add package-info to server

---
+++ server/main/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contains server implementations that encapsulate Contestants Bench, General Repository, Playground and Referee Site entities.
+ *
+ * @author Diogo Paiva (103183)
+ * @author João Fonseca (103154)
+ * @version 1.0
+ */
+package server.main;
